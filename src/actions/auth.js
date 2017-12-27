3aa2afe75bf82791139faa2380464764aff66909
Oleg Tsyba <oleg.tsyba.ua@gmail.com>
2017-12-27 19:52:52 +0000

    fixup! feature: add user jwt token to authorization header

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,20 +1,22 @@
 import * as authApi from "../api/auth";
 import decode from "jwt-decode";
 
-import { USER_LOGGED_IN } from "../constants";
+import { USER_LOGIN } from "../constants";
+import { setAuthHeader } from "../utils/auth_header";
 
 export function login(data) {
   return async dispatch => {
     const token = await authApi.login(data);
     const { email, name } = decode(token);
     window.localStorage.user_jwt = token;
+    setAuthHeader(token);
     dispatch(userLoggerIn({ email, name, token }));
   };
 }
 
 export function userLoggerIn(user) {
   return {
-    type: USER_LOGGED_IN,
+    type: USER_LOGIN,
     user
   };
 }
